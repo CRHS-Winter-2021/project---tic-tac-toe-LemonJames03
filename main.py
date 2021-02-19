af8f7eb7a92f6f09570810dc863aa37b98ea01fd
Garrett Allen <gaal1719@cloud.edu.pe.ca>
2021-02-19 03:34:42 +0000

    I have a question about the error on line 41. Once I fix that the code should be done.

--- main.py
+++ main.py
@@ -1,85 +1,86 @@
-##Tic Tac Toe
-#Name:
-#Date:
+import sys
 
-#1. (Var) Setup the empty board as a list
+
+done = 0
+turn = 0
 theBoard = []
 
-#2. (fun) Print the board.
-#in: a 10 item list (either x, o or ' ')
-#do: print a graphic for the board
-#out: none
 
-def printBoard(board):
-    pass
+
+
+
+
+
+
+def printBoard():
+  theboard = ('     |     |     \n  '+ theBoard[7] + '  |  '+ theBoard[8] + '  |  '+ theBoard[9] + '  \n_____|_____|_____\n     |     |     \n  '+ theBoard[4] + '  |  '+ theBoard[5] + '  |  '+ theBoard[6] + '  \n_____|_____|_____\n     |     |     \n  '+ theBoard[1] + '  |  '+ theBoard[2] + '  |  '+ theBoard[3] + '  \n     |     |     \n')
+  print(theboard)
 
 #3a. (fun) Determine if player is X or O
-player1 = ''
-player2 = ''
-
-#in: None
-#do: get user choice, assign X/O to player1 and 2
-#out: None
-
-def chooseLetter():
-    pass
 
 
-#3b. (fun) Choose starting player 1 or 2
-def chooseStart():
-    pass
-
-#4. (fun) Get player move
-#in: board as list, player as X or O
-#do: get user choice (1-9),
-#    check if the space is empty,
-#    update the board with the X or O at the user location
-#out: none
-
-def playerMove(board, player):
-    pass
-
-
-#5. (fun) Check Winner
-#in: board as list, player as X or O
-#do: check all possible win scenarios
-#out: True for win, False otherwise
+ 
+def play():
+  global turn
+ 
+  while done == 0:
+    if turn % 2 == 0:
+      letter = 'X'
+      print("X's Turn")
+    else:
+      letter = 'O'
+      print("O's Turn")
     
-def checkWin(board, player):
-    pass
+    move = input('Where would you like to make your move ')
+ 
+    while move not in ['1','2','3','4','5','6','7','8','9']  or  theBoard[int(move)] != ' ':
+      print('Can not do that')
+      move = (input('Where would you like to make your move ')
+ 
+      
+    theBoard[int(move)] = letter
+    
+    printBoard()
+    win()
+    draw()
+    turn += 1
+
+def win():
+ 
+ 
+  
+ 
+  if theBoard[1] == 'X' and theBoard[2] == 'X' and theBoard[3] == 'X'  or  theBoard[4] == 'X' and theBoard[5] == 'X' and theBoard[6] == 'X'  or  theBoard[7] == 'X' and  theBoard[8] == 'X' and theBoard[9] == 'X'  or  theBoard[7] == 'X' and theBoard[4] == 'X' and theBoard[1] == 'X'  or  theBoard[8] == 'X' and theBoard[5] == 'X' and theBoard[2] == 'X'  or  theBoard[9] == 'X' and theBoard[6] == 'X' and theBoard[3] == 'X'  or  theBoard[7] == 'X' and theBoard[5] == 'X' and theBoard[3] == 'X'  or  theBoard[9] == 'X' and theBoard[5] == 'X' and theBoard[1] == 'X':
+  
+    print('X Wins!')
+    sys.exit()
+  
+  if theBoard[1] == 'O' and theBoard[2] == 'O' and theBoard[3] == 'O'  or  theBoard[4] == 'O' and theBoard[5] == 'O' and theBoard[6] == 'O'  or  theBoard[7] == 'O' and theBoard[8] == 'O' and theBoard[9] == 'O'  or  theBoard[7] == 'O' and theBoard[4] == 'O' and theBoard[1] == 'O'  or  theBoard[8] == 'O' and theBoard[5] == 'O' and theBoard[2] == 'O'  or  theBoard[9] == 'O' and theBoard[6] == 'O' and theBoard[3] == 'o'  or  theBoard[7] == 'O' and theBoard[5] == 'O' and theBoard[3] == 'O'  or  theBoard[9] == 'O' and theBoard[5] == 'O' and theBoard[1] == 'O':
+    print('O Wins!')
+    sys.exit()
+
+def draw():
+  if theBoard.count(' ') == 0:
+    done = 1
+    print('Draw!')
+    sys.exit()
 
 
-#6. (fun) Check if board is full
-#Because there are 10 list items for 9 spots,
-#the first item theBoard[0] will always be ' '
-#in: board as list
-#do: count number of empty spaces, if there is no more spaces
-#out: return True if board is full, False otherwise
-
-def checkFull(board):
-    pass
-
-#7. Main function
 
 def main():
-    #print Welcome
-    #print instructions
-
-    #game play
-    #get player letter choice
+  global turn
+  global theBoard
+  print("Welcome to Garrett's Tic Tac Toe Mini Game")
+  print("The board is relative to the number pad on your key board")
     
-    #while board is not full
-    ###first player move
-        #player chooses move
-        #print board
-        #check win
-        #check board full
+  first = input('Player1 type 1 if you would like to be x and 2 for o ')
+  while first != 1 and first != 2:
+    input('You need to put 1 or 2.\n Type 1 for x and 2 for o')  
+    if first == '2':
+      turn += 1
+    theBoard = ['blank', '', '', '', '', '', '', '', '', '']
 
-    ###second player move
-        #player chooses move
-        #print baord
-        #check win
     
-    
-    pass
-
+main()
+printBoard()
+play()
